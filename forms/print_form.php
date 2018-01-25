154c0e6a31c5ba98200cf95650c716f17fef164a
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2018-01-25 10:58:13 -0300

    Fixed teachers query

--- forms/print_form.php
+++ forms/print_form.php
@@ -31,7 +31,8 @@ class paperattendance_print_form extends moodleform {
 		$mform = $this->_form;
 		$instance = $this->_customdata;		
 		$courseid = $instance["courseid"];
-		
+		$enrolincludes = explode("," ,$CFG->paperattendance_enrolmethod);
+		list($sqlin, $param) = $DB->get_in_or_equal($enrolincludes);
 		$teachersquery = "SELECT u.id, 
 							e.enrol,
 							CONCAT(u.firstname, ' ', u.lastname) AS name
@@ -42,10 +43,10 @@ class paperattendance_print_form extends moodleform {
 							INNER JOIN {context} ct ON (ct.id = ra.contextid)
 							INNER JOIN {course} c ON (c.id = ct.instanceid AND e.courseid = c.id)
 							INNER JOIN {role} r ON (r.id = ra.roleid)
-							WHERE ct.contextlevel = '50' AND r.id = 3 AND c.id = ? AND e.enrol = 'database'
+							WHERE ct.contextlevel = '50' AND r.id = 3 AND c.id = ? AND e.enrol $sqlin
 							GROUP BY u.id";
 		
-		$teachers = $DB->get_records_sql($teachersquery, array($courseid));
+		$teachers = $DB->get_records_sql($teachersquery, array($courseid, $param));
 		
 		$assistantsquery = "SELECT u.id,
 							e.enrol,
@@ -65,13 +66,11 @@ class paperattendance_print_form extends moodleform {
 		$arrayteachers = array();
 		$arrayteachers["no"] = get_string('selectteacher', 'local_paperattendance');
 		
-		$enrolincludes = explode("," ,$CFG->paperattendance_enrolmethod);		
-		
 		foreach ($teachers as $teacher){
 			
 			$enrolment = explode(",", $teacher->enrol);
 			// Verifies that the teacher is enrolled through a valid enrolment and that we haven't added him yet.
-			if (count(array_intersect($enrolment, $enrolincludes)) == 0 || isset($arrayteachers[$teacher->id])) {
+			if (isset($arrayteachers[$teacher->id])) {
 				continue;
 			}
 			$arrayteachers[$teacher->id] = $teacher->name;
